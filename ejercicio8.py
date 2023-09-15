2b5c621e92adc7e4f2126708315dcdf2c85d2879
Miqueler <miquel.parisi@campus.xaloc.org>
2023-09-15 19:18:47 +0200

    Create ejercicio8.py

---
+++ ejercicio8.py
@@ -0,0 +1,5 @@
+# programa que pida un número de horas y muestre por pantalla los minutos y segundos
+hours=int(input("Intoduce el numero de horas: "))
+min=hours*60
+secs=min*60
+print(f"El número de minutos es: {min} y en segundos es: {secs}")

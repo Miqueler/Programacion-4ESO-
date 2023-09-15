4676b5ccc6e527b434e3691cf4fdc140c76bdf84
Miqueler <miquel.parisi@campus.xaloc.org>
2023-09-15 19:39:26 +0200

    Create ejercicio11.py

---
+++ ejercicio11.py
@@ -0,0 +1,6 @@
+#Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva por pantalla en el área y el perímetro.
+x=int(input("Cuál es la longitud del lado del cuadrado?: "))
+area=x**2
+per=x*4
+print(f"El perímetro del cuadrado es: {per}")
+print(f"El área del cuadrado es: {area}")

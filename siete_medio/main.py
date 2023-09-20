c0a25cef233b5f45c7315dc225c889ebf47a2567
Miqueler <miquel.parisi@campus.xaloc.org>
2023-09-20 12:00:29 +0200

    Update main.py

--- siete_medio/main.py
+++ siete_medio/main.py
@@ -6,6 +6,7 @@ card=0
 x=True
 y=True
 res_lit=[]
+points=100
 while x:
     count=0
     while y:
@@ -18,31 +19,38 @@ while x:
         want_card=input("If you want a card type 0: ")
         if want_card=="0":
             count+=card
+            print(f"Your card was: {card}")
             print(f"Your current card count is: {count}")
 
         if count>7.5:
             print("You lost the game, better luck next time!")
             y=False
             res_lit.append(count)
+            points-=10
         if count==7.5:
                 print("Congratultions, you won the game!")
                 y=False
                 res_lit.append(count)
+                points+=10
         if want_card!="0":
             if count <6:
                 print("You could risk it a little more!")
                 y=False
                 res_lit.append(count)
+                points-=5
             elif count in [6,6.5,7]:
                 print("You were a little conservative!")
                 y=False
                 res_lit.append(count)
-        
-
-
-    finisher=input("If you want to end the game type end: ")
-    if finisher=="end":
-        x=False
+                points+=5
+    if points<=0:
+        x=False    
+        print("You ran out of points, better luck next time")
     else:
-        y=True
+        finisher=input("If you want to end the game type end: ")
+        if finisher=="end":
+            x=False
+            print(f"Nice game, you got {points} points!")
+        else:
+            y=True
 print(f"The result you got were: {res_lit}")

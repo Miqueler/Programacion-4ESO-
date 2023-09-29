2e330efd89dc9c9dc03277a8cd1a9812c8b9068c
Miqueler <miquel.parisi@campus.xaloc.org>
2023-09-29 11:58:41 +0200

    Create ejercicio19.py

---
+++ Ejercicios_iniciales/ejercicio19.py
@@ -0,0 +1,12 @@
+#. Programa que introduzca dos números y devuelva cuál de los dos es mayor, menor o son 
+#iguales
+
+x=input("Introduce el primer número: ")
+y=input("Introduce el segundo número: ")
+
+if x>y:
+    print("The first one is bigger")
+elif x<y:
+    print("The seci¡ond one is bigger")
+else:
+    print("They are equal")

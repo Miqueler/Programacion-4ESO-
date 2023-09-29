5f8a9209546258d87f4184625feed82b1e22dc91
Miqueler <miquel.parisi@campus.xaloc.org>
2023-09-29 11:53:02 +0200

    l

--- siete_medio/test.py
+++ siete_medio/test.py
@@ -2,6 +2,7 @@ text=open("registro.txt","r")
 w=0
 l=0
 d=0
+tg=0
 for i in text.read():
     if i=="2":
         w+=1
@@ -9,6 +10,11 @@ for i in text.read():
         l+=1
     if i=="1":
         d+=1
-print(f"You won {w} times, lost {l} times and drew {d} times")
+tg=w+l+d
+pw=round(w/tg*100,2)
+pl=round(l/tg*100,2)
+pd=round(d/tg*100,2)
 
+print(f"You won {w} times, lost {l} times and drew {d} times")
+print(f"You won {pw}% of times, lost {pl}% of times and drew {pd}% of times")
 text.close()

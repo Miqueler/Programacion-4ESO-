6fb2c1a3f27538ea4b9a8c988cd0c3d446e4b704
Miqueler <miquel.parisi@campus.xaloc.org>
2023-10-06 12:31:01 +0200

    Create ejercicio32.py

---
+++ Ejercicios_iniciales/ejercicio32.py
@@ -0,0 +1,13 @@
+#Cómo solucionarías con ayuda de métodos String el problema del ejercicio anterior para 
+#no distinguir entre mayúsculas y minúsculas
+
+a="A quién madruga Dios ayuda"
+z=input("Itroduce un texto: ")
+y=z.lower
+x=a.lower
+existe=x.find(y)
+
+if existe != -1:
+    print(f"La palabra está en la frase y está en el índice {existe}")
+else: 
+    print("La palabra no está en la frase")

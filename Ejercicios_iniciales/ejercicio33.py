265cebbddf1353d200e7fdefc67bf2ce0f337496
Miqueler <miquel.parisi@campus.xaloc.org>
2023-10-06 12:40:20 +0200

    Create ejercicio33.py

---
+++ Ejercicios_iniciales/ejercicio33.py
@@ -0,0 +1,10 @@
+#Programa un código que permita contar el número de vocales de la siguiente frase: No 
+#hay mal que dure cien años
+
+x="No hay mal que dure cien años aaaaa"
+a=x.count("a")
+e=x.count("e")
+i=x.count("i")
+o=x.count("o")
+u=x.count("u")
+print(f"El número de a es {a} el número de e es {e} el número de i es {i} el número de o es {o} y el número de u es {u}")

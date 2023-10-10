d0f1712f2458917ad7a30ff4d6f8f6ec9123cd40
Miqueler <miquel.parisi@campus.xaloc.org>
2023-10-10 12:55:21 +0200

    Update ejercicio34.py

--- Ejercicios_iniciales/ejercicio34.py
+++ Ejercicios_iniciales/ejercicio34.py
@@ -2,12 +2,14 @@
 #ejecute correctamente
 
 #inicializo valores a cada variable
-var_numero=6734
+var_numero="6735"
 var_suma=0
 #obtengo su longitud
 var_longitud=len(var_numero)
 #sumo cada digito a partir del índice de cada posición
-var_suma = var_numero [1] + var_numero [2]+ var_numero[3] + var_numero [4]
-#utilizo una condición y el operador aritmético // para saber si el resto da 0 y ver si es par
-if var_suma // 2 == 0:
+var_suma = int(var_numero[1]) + int(var_numero [2])+ int(var_numero[3]) + int(var_numero [0])
+#utilizo una condición y el operador aritmético % para saber si el resto da 0 y ver si es par
+if var_suma % 2 == 0:
+    print (f"el valor de {var_suma} es par")
+else:
     print (f"el valor de {var_suma} es impar")

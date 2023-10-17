c9b79731d91bc3683045b4df9543659a12305878
Miqueler <miquel.parisi@campus.xaloc.org>
2023-10-17 13:39:38 +0200

    l

--- Contraseña/contraseña_f2.py
+++ Contraseña/contraseña_f2.py
@@ -1,7 +1,14 @@
 run=True
 while run:
+    c1=0
+    c2=0
+    c3=0
+    c4=0
+    c5=0
+    c6=0
+    c7=0
     print('''Instruccions:
-        1.La contrasenya ha de tindre entre 6 i 8 caracters
+        1.La contrasenya ha de tindre 8 caracters
         2.Ha de tindre les següents condicions:
             Un número entre 1 i 5 inclosos
             Una lletra minúscula
@@ -12,22 +19,34 @@ while run:
             Un dels següents símbols &,/,#
             Un número menor o igual a 5''')
     password=input("Introduce la contraseña: ")
-    if len(password)>=6 and len(password) <=8:
+    if len(password)==8:
         for i in password:
-            if i >= 1 and i<=5:
-                print(1)
-            elif i.islower():
-                print(2)
-            elif i.isupper():
-                print(3)
+            #Comprovacions de nums
+            if i.isnumeric():
+                u=int(i)
+                if u >= 1 and u<=5:
+                    print(1)
+                    c1+=1
+                elif u>=6 and u<=9:
+                    print(5)
+                    c5+=1
+                elif u<=5:
+                    print(7)
+                    c7+=1
+            if i.isalpha():
+                if i.islower():
+                    print(2)
+                    c2+=1
+                elif i.isupper():
+                    print(3)
+                    c3+=1
             elif i in ["*","_","@"]:
                 print(4)
-            elif i>=6 and i<=9:
-                print(5)
+                c4+=1
+            
             elif i in ["&","/","#"]:
                 print(6)
-            elif i<=5:
-                print(7)
+                c6+=1
 
     else:
         print(f"Error, el password té una longitud de {len(password)} caràcters i no compleix els requisits")
--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
-x="FSJHjhsdh47"
-print(x.find(str(range(1,5))))
-print(range(1,6))
+hi="12345"
+for i in hi:
+    print(i)
+    print(i.isnumeric())

fac9ab8f8bb909ab11651e3011af3156ab1245d9
Miqueler <miquel.parisi@campus.xaloc.org>
2023-10-24 12:50:16 +0200

    l

--- Contraseña/FASE 3/contraseña_f3.py
+++ Contraseña/FASE 3/contraseña_f3.py
@@ -1,12 +1,6 @@
 from tkinter import *
 from checker import check
-root=Tk()
-root.title("Password checker")
-root.state("zoomed")
 
-t_box=Entry(root)
-t_box.config(font=("Calibri",30))
-t_box.place(x=585, y=200, width=350, height=60)
 def gathering(t_b_name):
     pasword=t_box.get()
     print(pasword)
@@ -16,10 +10,17 @@ def gathering(t_b_name):
     else:
         print("The password is incorrect")
 
+root=Tk()
+root.title("Password checker")
+root.state("zoomed")
+
+t_box=Entry(root)
+t_box.config(font=("Calibri",30))
+t_box.place(x=485, y=200, width=350, height=60)
 
 enter_button=Button(root, text="ENTER", command=lambda:gathering(t_box), padx=10, pady=10)
 enter_button.config(font=("Helvatica", 40))
-enter_button.place(x=950, y=200, width=150, height=60)
+enter_button.place(x=850, y=200, width=200, height=60)
 
 root.mainloop()
 

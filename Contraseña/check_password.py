13287cdc8d590e70e8f15c714b256e38c4c86a88
Miqueler <miquel.parisi@campus.xaloc.org>
2023-10-27 12:32:14 +0200

    l

--- Contraseña/check_password.py
+++ Contraseña/check_password.py
@@ -2,58 +2,64 @@ list_passwords=["2Aa*a","2Aa*a8#8a","9aA*a8#3","41A*a8#3","4ba*a8#3","4bA%a8#3",
 for i in list_passwords:
     password=i
     correct=True
+    errores=""
+
     if len(password)>=6 and len(password) <=8:
         #Pas 1
         try:
             if int(password[0])>5 or int(password[0]) <1:
-                print("El primer caracter ha d'estar entre 1 i 5")
+                errores+="Error en el caracter 1 "
                 correct=False
             
         except ValueError:
-            print("El primer caracter ha de ser un número")
+            errores+="Error en el caracter 1 "
             correct=False
         #Pas 2
         if not password[1].islower():
-            print("El segon caracter ha de ser una lletra en miniuscules")
+            errores+="Error en el caracter 2 "
             correct=False
         #Pas 3
         if not password[2].isupper():
-            print("El tercer caracter ha de ser una lletra en majuscules")
+            errores+="Error en el caracter 3 "
             correct=False
         #Pas 4
         if not password[3]in ["_", "@", "*"]:
-            print("El quart caracter ha de ser un dels següents caracters: (_,@,*)")
+            errores+="Error en el caracter 4 "
             correct=False
         #Pas 5
         if not password[4].islower():
-            print("El cinqué caracter ha de ser una lletra en minúscula")
+            errores+="Error en el caracter 5 "
             correct=False
         #Pas 6
         try:
             if not int(password[5])>=6 and int(password[5]) <=9:
-                print("Sisé caracter ha de ser un numero entre 6 i 9")
+                errores+="Error en el caracter 6 "
                 correct=False
         except ValueError:
-            print("El sisé caracter ha de ser un número")
+            errores+="Error en el caracter 6 "
             correct=False
         #Pas 7
         if len(password)>=7:
             if not password[6]in ["&", "/", "#"]:
-                print("El seté caracter ha de ser un dels següents caracters: (&,/,#)")
+                errores+="Error en el caracter 7 "
                 correct=False
         #Pas 8
         if len(password)>=8:
             try:
                 if not int(password[7]) <=5:
-                    print("El vuité caracter ha de ser un número entre 6 i 9")
+                    errores+="Error en el caracter 8 "
                     correct=False
             except ValueError:
-                print("El vuité caracter ha de ser un número")
+                errores+="Error en el caracter 8 "
                 correct=False
+        
+
     else:
         print(f"Error, el password té una longitud de {len(password)} caràcters i no compleix els requisits")
         correct=False
 
     if correct:
         print("La contraseña és correcta")
+    else:
+        print(errores)
     print("----------------------------------------------------")
--- Contraseña/contraseña.py
+++ Contraseña/contraseña.py
@@ -1,5 +1,5 @@
 correct=True
-
+errores=""
 print('''Instruccions:
         1.La contrasenya ha de tindre entre 6 i 8 caracters
         2.Forçar els següents valors en les següents posicions:
@@ -16,49 +16,49 @@ if len(password)>=6 and len(password) <=8:
     #Pas 1
     try:
         if int(password[0])>5 or int(password[0]) <1:
-            print("El primer caracter ha d'estar entre 1 i 5")
+            errores+="Error en el caracter 1 "
             correct=False
         
     except ValueError:
-        print("El primer caracter ha de ser un número")
+        errores+="Error en el caracter 1 "
         correct=False
     #Pas 2
     if not password[1].islower():
-        print("El segon caracter ha de ser una lletra en miniuscules")
+        errores+="Error en el caracter 2 "
         correct=False
     #Pas 3
     if not password[2].isupper():
-        print("El tercer caracter ha de ser una lletra en majuscules")
+        errores+="Error en el caracter 3 "
         correct=False
     #Pas 4
     if not password[3]in ["_", "@", "*"]:
-        print("El quart caracter ha de ser un dels següents caracters: (_,@,*)")
+        errores+="Error en el caracter 4 "
         correct=False
     #Pas 5
     if not password[4].islower():
-        print("El cinqué caracter ha de ser una lletra en minúscula")
+        errores+="Error en el caracter 5 "
         correct=False
     #Pas 6
     try:
         if not int(password[5])>=6 and int(password[5]) <=9:
-            print("Sisé caracter ha de ser un numero entre 6 i 9")
+            errores+="Error en el caracter 6 "
             correct=False
     except ValueError:
-        print("El sisé caracter ha de ser un número")
+        errores+="Error en el caracter 6 "
         correct=False
     #Pas 7
     if len(password)>=7:
         if not password[6]in ["&", "/", "#"]:
-            print("El seté caracter ha de ser un dels següents caracters: (&,/,#)")
+            errores+="Error en el caracter 7 "
             correct=False
     #Pas 8
     if len(password)>=8:
         try:
             if not int(password[7]) <=5:
-                print("El vuité caracter ha de ser un número entre 6 i 9")
+                errores+="Error en el caracter 8 "
                 correct=False
         except ValueError:
-            print("El vuité caracter ha de ser un número")
+            errores+="Error en el caracter 8 "
             correct=False
 else:
     print(f"Error, el password té una longitud de {len(password)} caràcters i no compleix els requisits")
@@ -66,3 +66,5 @@ else:
 
 if correct:
     print("La contraseña és correcta")
+else:
+    print(errores)

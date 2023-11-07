14f29b2b8dd7c29fce2483cd422facabd2fd6ab9
Miqueler <miquel.parisi@campus.xaloc.org>
2023-11-07 13:01:20 +0100

    Update ejercicio45.py

--- Ejercicios_iniciales/ejercicio45.py
+++ Ejercicios_iniciales/ejercicio45.py
@@ -2,6 +2,18 @@
 #distinguiendo vocales y las consonantes:
 voc=""
 con=""
+error=0
 x=input("Introduce tu palabra: ")
 for i in range(len(x)):
-    if x[i].
+    if x[i] in 'aeiouàèìòùáéíóú':
+        voc+=x[i]
+    elif x[i] in 'bcdfghjklmnñpqrstvwxyz':
+        con+=x[i]
+    else:
+        con+=x[i]
+        error+=1
+if error!=0:
+    con+=" ¿?"
+
+print(f"Las vocales de la palabra Abrigo son: {voc}")
+print(f"Las consonantes de la palabra Abrigo son: {con}")

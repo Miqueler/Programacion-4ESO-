caa7d08ab819c5bc83a037be56198f858f44e65a
Miqueler <miquel.parisi@campus.xaloc.org>
2023-11-22 16:46:38 +0100

    l

--- HUNDIR LA FLOTA/main.py
+++ HUNDIR LA FLOTA/main.py
@@ -11,12 +11,12 @@ i=["*","*","*","*","*","*","*","*","*","*"]
 j=["*","*","*","*","*","*","*","*","*","*"]
 #1*5, 1*4, 2*3, 1*2
 def get_boat_pos():
-    boat_pos=[]
+    boat_pos=""
 
     row=""
     while row not in ["a","b","c","d","e","f","g","h","i","j"]:
             row=input("In what row do you want to place your boat (a-j): ")
-    
+
     corr=False
     x=""
     while corr==False:
@@ -25,17 +25,11 @@ def get_boat_pos():
             index=int(x)-1
             if index>=0 and index<=9:
                 corr=True
-    
-    direction=""
-    while direction not in ["up","down","left","right"]:
-            direction=input("In what direction do you want to place the boat (up, down, right, left): ")
-    boat_pos.append(row)
-    boat_pos.append(index)
-    boat_pos.append(direction)
+
+    boat_pos+=row
+    boat_pos+=str(index)
     return boat_pos
 
-def place_boat(boat_lenght,a,b,c,d,e,f,g,h,i,j):
-    boat_pos=get_boat_pos()
-    if boat_pos[2]=="right":
-            pass
+def place_boat(boat_lenght):
+    if boat_lenght.
 print(get_boat_pos())

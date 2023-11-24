d1b49bdbe6c92c3ebcb821b163e06120bb87d861
Miqueler <miquel.parisi@campus.xaloc.org>
2023-11-24 20:50:51 +0100

    Create map_display.py

---
+++ HUNDIR LA FLOTA/map_display.py
@@ -0,0 +1,5 @@
+#Have to code a way to store the coords where boats have been hit
+#The bot display only has to show where you have attacked and where you have hit
+
+def print_map(hit_coords,boat_cooords):
+    pass

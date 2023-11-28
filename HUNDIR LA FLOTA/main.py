273bb2616bb66424039492e59a7ef43c9533d2e6
Miqueler <miquel.parisi@campus.xaloc.org>
2023-11-28 13:29:27 +0100

    l

--- HUNDIR LA FLOTA/main.py
+++ HUNDIR LA FLOTA/main.py
@@ -2,6 +2,7 @@ from player_map_main_logic import *
 from computer_map_main_logic import *
 from player_main_game_fun import *
 from computer_main_game_fun import *
+from map_display import *
 import time
 #1*5, 1*4, 2*3, 1*2
 #Have to program check placed boats
@@ -11,14 +12,19 @@ print("PLACE THE 5 LENGHT BOAT")
 boat_pos=get_boat_pos()
 direction=get_boat_direction(5,boat_pos)
 player_boat_placement.append(place_boat(5,boat_pos,direction))
+print_map([],player_boat_placement,[])
 print("PLACE THE 4 LENGHT BOAT")
 player_boat_placement.append(check_if_placement_possible(player_boat_placement,4))
+print_map([],player_boat_placement,[])
 print("PLACE THE 3 LENGHT BOAT")
 player_boat_placement.append(check_if_placement_possible(player_boat_placement,3))
+print_map([],player_boat_placement,[])
 print("PLACE THE 3 LENGHT BOAT")
 player_boat_placement.append(check_if_placement_possible(player_boat_placement,3))
+print_map([],player_boat_placement,[])
 print("PLACE THE 2 LENGHT BOAT")
 player_boat_placement.append(check_if_placement_possible(player_boat_placement,2))
+print_map([],player_boat_placement,[])
 print(player_boat_placement)
 
 print("NOW THE BOT IS GOING TO PLACE HIS BOATS")
@@ -36,8 +42,14 @@ print(time.time()-time_start)
 print("THE BOATS HAVE BEEN SET, THE WAR MUST BEGIN")
 player_hits_list=[]
 bot_hits_list=[]
+player_bot_boat_hit=[]
+bot_player_boat_hit=[]
 x=True
 while x:
+    print("YOUR MAP WITH THE BOT'S HITS")
+    print_map(bot_hits_list,player_boat_placement,bot_player_boat_hit)
+    print("THE BOT'S MAP WITH YOUR HITS")
+    print_map(player_hits_list,[],player_bot_boat_hit)
     current_player_hit=choose_hit(player_hits_list)
     player_hits_list.append(current_player_hit)
     player_hit_result=find_hit_result(bot_boat_placement,current_player_hit)
@@ -45,6 +57,7 @@ while x:
     
     if player_hit_result != "":
         print("HIT")
+        player_bot_boat_hit.append(current_player_hit)
         single_boat_list=bot_boat_placement[int(player_hit_result[0])]
         single_boat_list.pop(int(player_hit_result[1]))
         for i in bot_boat_placement:
@@ -65,6 +78,7 @@ while x:
     
     if bot_hit_result != "":
         print("The bot has hitten one of your boats!")
+        bot_player_boat_hit.append(current_bot_hit)
         single_boat_list=player_boat_placement[int(bot_hit_result[0])]
         single_boat_list.pop(int(bot_hit_result[1]))
         for i in player_boat_placement:
--- HUNDIR LA FLOTA/map_display.py
+++ HUNDIR LA FLOTA/map_display.py
@@ -1,5 +1,27 @@
 #Have to code a way to store the coords where boats have been hit
 #The bot display only has to show where you have attacked and where you have hit
 
-def print_map(hit_coords,boat_cooords):
-    pass
+def print_map(hit_coords,boat_cooords,successfull_hits):
+    map_layout=[["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"],
+                ["*","*","*","*","*","*","*","*","*","*"]]
+    for i in boat_cooords:
+        for x in i:
+            map_layout[int(x[0])][int(x[1])]="@"
+    if hit_coords!=[]:
+        for i in hit_coords:
+            map_layout[int(i[0])][int(i[1])]="/"
+    if successfull_hits!=[]:
+        for i in successfull_hits:
+                map_layout[int(i[0])][int(i[1])]="#"
+    for i in map_layout:
+        print(i)
+    return map_layout
+

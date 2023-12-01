5b00efde8f91860beccb8e2fd6978a9b93c82cf0
Miqueler <miquel.parisi@campus.xaloc.org>
2023-12-01 12:14:45 +0100

    l

--- HUNDIR LA FLOTA/map_display.py
+++ HUNDIR LA FLOTA/map_display.py
@@ -12,6 +12,7 @@ def print_map(hit_coords,boat_cooords,successfull_hits):
                 ["*","*","*","*","*","*","*","*","*","*"],
                 ["*","*","*","*","*","*","*","*","*","*"],
                 ["*","*","*","*","*","*","*","*","*","*"]]
+    all_letters_map=["a","b","c","d","e","f","g","h","i","j"]
     for i in boat_cooords:
         for x in i:
             map_layout[int(x[0])][int(x[1])]="@"
@@ -21,7 +22,10 @@ def print_map(hit_coords,boat_cooords,successfull_hits):
     if successfull_hits!=[]:
         for i in successfull_hits:
                 map_layout[int(i[0])][int(i[1])]="#"
+    print(" [/1/ ,/2/ ,/3/ ,/4/ ,/5/ ,/6/ ,/7/ ,/8/ ,/9/,/10/]")
+    y=0
     for i in map_layout:
-        print(i)
+        print(f"{all_letters_map[y]}{i}")
+        y+=1
     return map_layout
 
--- testing.py
+++ testing.py
@@ -1,5 +1,17 @@
-x=[[2,3],[1]]
-a="01"
-y=x[int(a[0])]
-y.pop(int(a[1]))
-print(x)
+print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+def print_format_table():
+    """
+    prints table of formatted text format options
+    """
+    for style in range(8):
+        for fg in range(30,38):
+            s1 = ''
+            for bg in range(40,48):
+                format = ';'.join([str(style), str(fg), str(bg)])
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+            print(s1)
+        print('\n')
+
+print_format_table()
+
+print('\x1b[3;32;40m'+"Hi"+'\x1b[0m')

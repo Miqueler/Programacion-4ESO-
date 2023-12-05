82c3e4e6f0be207bfa3763aff63599b2a7345b44
Miqueler <miquel.parisi@campus.xaloc.org>
2023-12-05 13:38:48 +0100

    l

--- testing3.py
+++ testing3.py
@@ -1,3 +1,10 @@
 import random
-for i in range(1000):
-    print(random.randint(0,9))
+import os
+
+#import csv
+#
+#data = open("C:\\Users\ParisiSauraMiquel\Desktop/randomcom/Nuevo Text Document.txt")
+#data = csv.reader(data)
+#print(os.path.isfile(data))
+
+os.remove("C:\\Users\ParisiSauraMiquel\Desktop/randomcom")

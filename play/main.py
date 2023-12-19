0b02eb89699b2f9e7b0b6fa4ffaeec957a87deef
Miqueler <miquel.parisi@campus.xaloc.org>
2023-12-19 13:23:02 +0100

    Update main.py

--- play/main.py
+++ play/main.py
@@ -1,25 +1,28 @@
 
 import pygame
+from random import randint
 from sys import exit
 
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('33')
+pygame.display.set_caption('Not Hypixel Skyblock')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("play/font/Pixeltype.ttf", 50)
 
 #Create textures#
-sky_surface = pygame.image.load('play/graphics/sky.png').convert()
-ground_surface = pygame.image.load('play/graphics/ground.png') .convert()
-score_surface = test_font.render('My Game', False, (64, 64, 64))
+sky_surface = pygame.image.load('play/graphics/sky.png').convert_alpha()
+ground_surface = pygame.image.load('play/graphics/ground.png').convert_alpha()
+score_surface = test_font.render('0', False, (64, 64, 64))
 score_rect = score_surface.get_rect(center=(400, 50))
 game_over=test_font.render("Game Over!", False, (64,64,64))
 game_over_rect=game_over.get_rect(center=(400,50))
 
 snail_surface = pygame.image.load('play/graphics/snail/snail1.png').convert_alpha()
-
 snail_rectangle = snail_surface.get_rect(midbottom=(600, 300))
+
+fly_surface = pygame.image.load('play\graphics\Fly\Fly1.png').convert_alpha()
+fly_rectangle=fly_surface.get_rect(midbottom=(600, 200))
 
 player_surface = pygame.image.load('play/graphics/Player/player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom=(80, 300))
@@ -30,6 +33,10 @@ ground_rectangle= ground_surface.get_rect()
 jump_counter = 0
 player_current_surface = 0
 snail_texture_counter=0
+snail_speed=20
+fly_texture_counter=0
+fly_speed=10
+score=0
 loose=False
 #BG MUSIC#
 pygame.mixer.music.load('play/audio/music.wav')
@@ -44,21 +51,28 @@ while True:
             exit()
     keys = pygame.key.get_pressed()
 
-
-
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10 )
-    if loose==False:
-        screen.blit(score_surface, (score_rect))
-    screen.blit(snail_surface, snail_rectangle)
-
+    if not loose:
+        score_surface = test_font.render(f'{score}', False, (64, 64, 64))
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect,20)
+        screen.blit(score_surface, score_rect)
+        screen.blit(snail_surface, snail_rectangle)
+        screen.blit(fly_surface,fly_rectangle)
+        screen.blit(player_surface, player_rectangle)
+    if loose:
+        pass
     
     if snail_rectangle.right < 0:
         snail_rectangle.left = 800
-    screen.blit(player_surface, player_rectangle)
+        snail_speed=randint(20,40)
+        score+=1
+    if fly_rectangle.right < 0:
+        fly_rectangle.left = 800
+        fly_speed=randint(10,30)
+        score+=2
+    
 
     
 # JUMP#
@@ -90,6 +104,13 @@ while True:
         player_current_surface=0
 #COLISIONS
     if pygame.Rect.colliderect(player_rectangle, snail_rectangle):
+        pygame.draw.rect(screen, '#c0e8ec', game_over_rect)
+        pygame.draw.rect(screen, '#c0e8ec', game_over_rect,10)
+        screen.blit(game_over, (game_over_rect))
+        loose=True
+    if pygame.Rect.colliderect(player_rectangle, fly_rectangle):
+        pygame.draw.rect(screen, '#c0e8ec', game_over_rect)
+        pygame.draw.rect(screen, '#c0e8ec', game_over_rect,10)
         screen.blit(game_over, (game_over_rect))
         loose=True
 
@@ -116,9 +137,16 @@ while True:
         snail_surface = pygame.image.load("play/graphics/snail/snail1.png").convert_alpha()
     if snail_texture_counter==14 and loose==False:
         snail_surface = pygame.image.load("play/graphics/snail/snail2.png").convert_alpha()
-        snail_rectangle.x-=20
+        snail_rectangle.x-=snail_speed
     snail_texture_counter-=1
-
+#FLY
+    if fly_texture_counter==0 and loose==False:
+        fly_texture_counter=8
+        fly_surface = pygame.image.load("play\graphics\Fly\Fly1.png").convert_alpha()
+    if fly_texture_counter==4 and loose==False:
+        fly_surface = pygame.image.load("play\graphics\Fly\Fly2.png").convert_alpha()
+        fly_rectangle.x-=fly_speed
+    fly_texture_counter-=1
 
     pygame.display.update()
     clock.tick(60)

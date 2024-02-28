0a06efde31614f6c9d3dc03ce1268b32294262c4
Miqueler <miquel.parisi@campus.xaloc.org>
2024-02-28 21:39:06 +0100

    l

---
+++ HP Wars Excercices/2022 exercices/ex30.py
@@ -0,0 +1,45 @@
+x=""
+teams=[]
+bikers=[]
+brands=[]
+while x!=["#"]:
+    x=input().split()
+    if x!=["#"]:
+        #Creates a list inside the bikers list with [biker_name,biker_points,biker's_team,biker's_brand]
+        bikers.append([x[0],0,x[1],x[2]])
+        if x[1] not in teams:
+            teams.append([x[1],0])
+        if x[2] not in brands:
+            brands.append([x[2],0])
+x=""
+while x!="#":
+    x=input()
+    if x!="#":
+        x=x.split("_")
+        fast_lap=x[-1].split("|")
+        x.pop(-1);x.append(fast_lap[0]);fast_lap.pop(0)
+        point_rew=8
+        for z in x:
+            team=bikers[bikers.index(z)+2]
+            brand=bikers[bikers.index(z)+3]
+            bikers[bikers.index(z)+1]+=point_rew
+
+            count_t=0
+            while teams[count_t][0]!=team:
+                count_t+=1
+            brands[count_t][1]+=point_rew
+            count_b=0
+            while brands[count_b][0]!=brand:
+                count_b+=1
+                teams[count_b][1]+=point_rew
+
+
+            point_rew-=1
+            if fast_lap in x:
+                bikers[bikers.index(z)+1]+=1
+                brands[count_t][1]+=1
+                teams[count_b][1]+=point_rew
+
+print(bikers)
+print(brands)
+print(teams)
--- testing3.py
+++ testing3.py
@@ -1,1 +1,3 @@
-print(chr(64))
+a=[1,2,3]
+a.pop(-1)
+print(a)

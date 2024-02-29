c59f11fafe5185bfef0dad1d477d6e54dcb663de
Miqueler <144797283+Miqueler@users.noreply.github.com>
2024-02-29 14:11:13 +0100

    Update ex30.py

--- HP Wars Excercices/2022 exercices/ex30.py
+++ HP Wars Excercices/2022 exercices/ex30.py
@@ -18,7 +18,7 @@ while x!="#":
         x=x.split("_")
         fast_lap=x[-1].split("|")
         x.pop(-1);x.append(fast_lap[0]);fast_lap.pop(0)
-        point_rew=8
+        point_rew=10
         for z in x:
             team=bikers[bikers.index(z)+2]
             brand=bikers[bikers.index(z)+3]
@@ -38,7 +38,10 @@ while x!="#":
                 bikers[bikers.index(z)+1]+=1
                 brands[count_t][1]+=1
                 teams[count_b][1]+=point_rew
-            point_rew-=1
+            if point_rew>4:
+                point_rew-=2
+            else:
+                point_rew-=1
 
 print(bikers)
 print(brands)

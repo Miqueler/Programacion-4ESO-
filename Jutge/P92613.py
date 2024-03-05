26af4be564b5b650c8efec072d1e576352e626d3
Miqueler <miquel.parisi@campus.xaloc.org>
2024-03-05 20:17:03 +0100

    Update P92613.py

--- Jutge/P92613.py
+++ Jutge/P92613.py
@@ -3,5 +3,8 @@ try:
     x=float(input())
 except ValueError:
     x=int(input())
-if str(x).split(".")[2]=="5": x+=0.1
+try:
+    if str(x).split(".")[1]=="5": x+=0.1
+except:
+    pass
 print(f"{math.floor(x)} {math.ceil(x)} {int(round(x,0))}")

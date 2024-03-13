ff37cf54fc98507b807561d6acf31d28b140f07d
Miqueler <miquel.parisi@campus.xaloc.org>
2024-03-13 20:05:56 +0100

    l

--- Traductor_verbos/main.py
+++
@@ -1,8 +0,0 @@
-class Dog():
-    def __init__(self):
-        pass
-    def bark(self):
-        self.bark=print("woof")
-
-tim=Dog()
-tim.bark()
---
+++ VERBOS/interfaz.py
@@ -0,0 +1,13 @@
+from main import *
+from tkinter import *
+
+root=Tk()
+root.title("IRREGULAR VERBS")
+root.state("zoomed")
+
+label1=Label(root, text="CHOOSE AN OPTION", font=("Pixel LCD7", 25))
+label1.place(x=250,y=30,width=1200, height=60)
+label2=Label(root, text="SUPERCALIFRAGILHISTICOESPIALIDOSO ",font=("Pixel LCD7",30))
+
+
+root.mainloop()

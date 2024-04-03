df82a5e4a8a0f6bd60ef21c570d68e5fe1a01860
Miqueler <miquel.parisi@campus.xaloc.org>
2024-04-03 12:44:07 +0200

    l

--- AHORCADO/main.py
+++ AHORCADO/main.py
@@ -1,19 +1,41 @@
 from random import choice
 import time
+
+#añadir el registro de las partidas a un txt
 def añade_verbos():
     file=open("AHORCADO\dic.txt","a")
     final=input("Introduce la palabra que quieres añadir: ").lower()
     file.write(f"\n{final}")
     file.close()
-
-    #sort an refine the words list so that there are no dashes or correct the problem
-    #eliminar palabras de la lista para que no se repitan
+def elimina_verbos():
+    fichero=open("AHORCADO\dic.txt")
+    dictionary=fichero.readlines()
+    user_verb=input("Introduce la palabra que quieres eliminar: ").lower()
+    c=0
+    linea=""
+    while c<len(dictionary):
+        if dictionary[c]==user_verb:
+            linea=dictionary[c].split(";")
+            break
+        c+=1
+    if linea=="":
+        print("La palabra no está en el diccionario")
+    else:
+        print(f"El verbo que quieres eliminar es:")
+        print(dictionary[c])
+        if input("Estas seguro (s): ").lower()=="s":
+            dictionary.pop(c)
+            file=open("AHORCADO\dic.txt","w")
+            file.writelines(dictionary)
+            file.close()
 
 x="s"
 add=input("Quieres añadir alguna palabra al diccionario?(s/n): ")
 if add == "s":
     añade_verbos()
-
+delete=input("Quieres eliminar alguna palabra al diccionario?(s/n): ")
+if delete=="s":
+    elimina_verbos()
 
 with open("AHORCADO\dic.txt","r") as d:
     lista_palabrasecreta=d.read().splitlines()
@@ -30,6 +52,7 @@ while x.lower()=="s":
     ahor=["A","H","O","R","C","A","D","O"]
     for i in range(len(palabrasecreta)):
         lista_partida.append("_")
+    print(lista_partida)
     guess=""
     while lista_ahorcado!=["A","H","O","R","C","A","D","O"] and lista_partida!=list(palabrasecreta):
         local_test=True
@@ -60,5 +83,13 @@ while x.lower()=="s":
     else:
         print("Enhorabuena, has ganado")
     print(f"Has acertado {len(aciertos)} veces y has fallado {len(errores)} veces")
-    print(f"Has tardado {round(time.time()-tem,1)}")
+    print(f"Has tardado {round(time.time()-tem,1)} segundos")
+
+    lista_palabrasecreta.pop(lista_palabrasecreta.index(palabrasecreta))
+    file=open("AHORCADO\dic.txt","w")
+    lista_palabrasecreta.sort()
+    for i in range(len(lista_palabrasecreta)):
+        file.write(f"{lista_palabrasecreta[i]}\n")
+    file.close()
+
     x=input("Quieres volver a jugar? (s/n): ")
